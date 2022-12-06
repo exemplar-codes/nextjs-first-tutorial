89347c85d0516a1269daa78b3ca0d7bc8dab744e
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-12-07 01:56:54 +0530

    Demo Next's Link

---
+++ pages/news/help.js
@@ -0,0 +1,5 @@
+function NewsHelpPage() {
+  return <h1>News help page</h1>;
+}
+
+export default NewsHelpPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 function AllNewsPage() {
-  return <h1>News items</h1>;
+  return (
+    <div>
+      <h1>News items</h1>
+      <Link href="/news/help">Need help?</Link>
+    </div>
+  );
 }
 
 export default AllNewsPage;
